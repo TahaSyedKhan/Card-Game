735ac72172e9cd6eaf15315d7799d584bdb4745a
TahaSyedKhan <tahasyedkhan123@gmail.com>
2026-02-02 15:12:46 +0530

    calculated score values for user and computer and also displayed remaining cards in deck and when 0 cards left to draw displayed the final winner

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const cards = document.getElementById('cards')
 const newDeck = document.getElementById('new-deck')
 const drawCards = document.getElementById('draw-cards')
+const cardNo = document.getElementById('card-no')
+const headerText = document.getElementById('header-text')
 
 let deckId
+let computerScore = 0
+let myScore = 0
 
 newDeck.addEventListener('click', () => {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
     .then(res => res.json())
     .then(data => {
         deckId = data.deck_id
+        cardNo.textContent = data.remaining
     })
 })
 
@@ -17,8 +22,42 @@ drawCards.addEventListener('click', () => {
     fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
         .then(res => res.json())
         .then(data => {
-            console.log(data)
+            cardNo.textContent = data.remaining
             cards.children[0].innerHTML = `<img class='cards' src='${data.cards[0].image}'/> ` 
             cards.children[1].innerHTML = `<img class='cards' src='${data.cards[1].image}'/>`
+
+            const handText =  determineWinner(data.cards[0], data.cards[1])
+            headerText.textContent = handText
+
+            document.getElementById('computer-score').textContent = computerScore
+            document.getElementById('my-score').textContent = myScore
+
+            if(data.remaining === 0) {
+                drawCards.disabled = true
+
+                if(computerScore > myScore) {
+                    headerText.textContent = "Computer wins!"
+                } else if (computerScore < myScore) {
+                    headerText.textContent = "You win!"
+                } else {
+                    headerText.textContent = "It's a tie"
+                }
+            }
         })
 })
+
+function determineWinner(card1, card2) {
+    const values = ["2", "3", "4", "5", "6", "7", "8", "9","10", "JACK", "QUEEN", "KING", "ACE"]
+    const card1Value = values.indexOf(card1.value)
+    const card2Value = values.indexOf(card2.value)
+
+    if(card1Value > card2Value) {
+        computerScore++
+        return "Computer's Hand"
+    } else if (card1Value < card2Value) {
+        myScore++
+        return "Your Hand"
+    } else {
+        return "War!"
+    }
+}
